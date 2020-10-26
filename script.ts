34ea3f95008ed124daf396b69a937c333a236b05
bhara <bharathJ222000@gmail.com>
2020-10-26 23:08:39 +0530

    started

--- script.ts
+++ script.ts
@@ -2,9 +2,11 @@ let startButton = document.getElementById('start');
 let cskHit = document.getElementById('csk-hit');
 let miHit = document.getElementById('mi-hit');
 let result = document.getElementById('result');
+var table = null;
 
 function start() {
     startButton.setAttribute('disabled','true');
     result.setAttribute('disabled','true');
     miHit.setAttribute('disabled','true');
+
 }
